7fa1b121793a021be779d33bd26d96203c889edf
Aya Maree <114161095+Aya-Maree@users.noreply.github.com>
2026-09-05 16:20:39 -0400

    Clean up application startup

--- TaskManagementApi/Program.cs
+++ TaskManagementApi/Program.cs
@@ -1,16 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using TaskManagementApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+// add controllers for the api
+builder.Services.AddControllers();
+
+// connect to the sqlite database
 builder.Services.AddDbContext<TaskDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddControllers();
-
 var app = builder.Build();
+
+// make sure the database is set up and has sample data
 using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<TaskDbContext>();
@@ -19,37 +21,7 @@ using (var scope = app.Services.CreateScope())
     DataSeeder.Seed(context);
 }
 
+// map the api controllers
 app.MapControllers();
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
-
-app.UseHttpsRedirection();
-
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
 
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
